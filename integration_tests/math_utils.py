dcc0bdd2f73b1d84de19695281a8112b429a701e
Jkim <jkimak1124@gmail.com>
2026-01-03 10:11:00 +0900

    Integration test: division_by_zero

---
+++ integration_tests/math_utils.py
@@ -0,0 +1,5 @@
+"""Mathematical utility functions."""
+
+def calculate_ratio(numerator: int, denominator: int) -> float:
+    """Calculate the ratio of two numbers."""
+    return numerator / denominator
